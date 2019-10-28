4ba0c23e392ce399c95cc949bc55f3fab877786a
dinoumoukeloo <56556162+dinoumoukeloo@users.noreply.github.com>
2019-10-28 20:03:18 +0100

    Update ready.js

--- .gitignore/Events/ready.js
+++ .gitignore/Events/ready.js
@@ -2,6 +2,6 @@
 module.exports = async(client) => {
     
     client.user.setStatus('dnd'); //Telling discord we are online.
-    client.user.setActivity("★☆French Origin☆★"); //Setting our dicord "playing" status to a defined text
+    client.user.setActivity("★☆4D☆★"); //Setting our dicord "playing" status to a defined text
     console.log(`Logged in as ${client.user.tag}`); //Sending a message into the console saying that we are logging in as (usertag). Can be usefull when you have multiple bots running on the same machine
 };
