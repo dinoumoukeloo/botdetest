45bbb9ec783cd89e1e62fa77db9133e4628c1861
dinoumoukeloo <56556162+dinoumoukeloo@users.noreply.github.com>
2019-10-28 13:37:25 +0100

    Add files via upload

---
+++ .gitignore/Commandes/ping.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+
+
+module.exports.run = (client, message, args) => {
+
+    let début = Date.now();
+
+    message.channel.send('Ping')
+
+        .then((m) => m.edit(`Pong : **${Date.now() - début}**ms`));
+
+};
+
+
+
+module.exports.help = {
+
+    name: 'ping'
+
+};
